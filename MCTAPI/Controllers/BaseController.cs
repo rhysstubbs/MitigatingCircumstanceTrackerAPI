59a0167295fb61e96f7a934ec11ac91502799e10
Rhys Stubbs <rhys@soundintheory.co.uk>
2018-10-03 11:24:54 +0100

    Bootstrap files for API development

---
+++ MCTAPI/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RESTAPI.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
--- MCTAPI/Program.cs
+++ MCTAPI/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MCTAPI
+namespace RESTAPI
 {
     public class Program
     {
