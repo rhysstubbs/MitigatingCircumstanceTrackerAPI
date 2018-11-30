c4a33a568f141d947adca68244f171256d45d739
Rhys Stubbs <rhys@soundintheory.co.uk>
2018-11-30 11:28:20 +0000

    Add basic test action for service status

--- API/Controllers/BaseController.cs
+++ API/Controllers/BaseController.cs
@@ -3,7 +3,14 @@
 namespace RESTAPI.Controllers
 {
     [ApiController]
+    [Route("/test")]
     public class BaseController : ControllerBase
     {
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
     }
 }
